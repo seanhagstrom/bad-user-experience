db40135c21c02448866fe206c7fe8bf9f2bd12d9
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2023-01-29 15:37:11 -0500

    feat: base functionality achieved

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+let count = 0;
+
 const badButton = document.querySelector('#but-why');
 
-badButton.addEventListener('mouseover', () =>
-  console.log('mouse has found the button!')
-);
+badButton.addEventListener('mouseover', () => {
+  if (count < 5) {
+    count++;
+    console.log(`count is: ${count}`);
+    badButton.style.position = 'relative';
+    badButton.style.left = `${getRandomInt(0, 45)}vw`;
+    badButton.style.top = `${getRandomInt(0, 45)}vh`;
+    console.dir(badButton.style);
+  }
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
