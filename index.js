a289cb3559c0f8106592339dd35155f7810065bf
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2023-02-02 16:54:31 -0500

    feat(bad button): adds full screen movement

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 let count = 0;
+const dodges = getRandomInt(1, 12);
 
+/**DOM SELECTORS**/
 const badButton = document.querySelector('#but-why');
 
+/**EVENT LISTENERS**/
 badButton.addEventListener('mouseover', () => {
-  if (count < 5) {
+  if (count < dodges) {
     count++;
     console.log(`count is: ${count}`);
-    badButton.style.position = 'relative';
-    badButton.style.left = `${getRandomInt(0, 45)}vw`;
-    badButton.style.top = `${getRandomInt(0, 45)}vh`;
-    console.dir(badButton.style);
+    const leftOrRight = whichWay('x');
+    const upOrDown = whichWay('y');
+
+    const styleString = `position: relative;
+      ${leftOrRight}: ${getRandomInt(0, 45)}vw;
+      ${upOrDown}: ${getRandomInt(0, 45)}vh;`;
+
+    console.log(styleString);
+    badButton.style = styleString;
+
+    console.dir(badButton);
   }
 });
 
+/**HELPER FUNCTIONS**/
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function whichWay(axis) {
+  if (axis === 'x') {
+    return getRandomInt(0, 1) === 0 ? 'left' : 'right';
+  }
+  if (axis === 'y') {
+    return getRandomInt(0, 1) === 0 ? 'top' : 'bottom';
+  }
 }
